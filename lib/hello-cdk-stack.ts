07e35e31ec1122d0501e0bc0468c4618fe7324e2
Er Vo <ervin.vojt@proton.me>
2024-03-16 21:34:34 +0100

    first CDK stack

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { join } from 'path';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const handler = new Function(this, 'Hello-lambda', {
+      runtime: Runtime.NODEJS_16_X,
+      memorySize: 512,
+      handler: 'app.handler',
+      code: Code.fromAsset(join(__dirname, '../lambdas'))
+    } )
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
